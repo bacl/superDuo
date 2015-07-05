bc27c270df6dcb65e6db3b5510ab3fd4513239e9
Bruno <baclpt@gmail.com>
2015-07-05 19:37:23 +0100

    Fix UP button back stack  on about activity

--- Football Scores/app/src/main/java/barqsoft/footballscores/AboutActivity.java
+++ Football Scores/app/src/main/java/barqsoft/footballscores/AboutActivity.java
@@ -1,5 +1,6 @@
 package barqsoft.footballscores;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -34,5 +35,10 @@ public class AboutActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             return inflater.inflate(R.layout.fragment_about, container, false);
         }
+    }
+    @Override
+    public Intent getParentActivityIntent() {
+        // UP/HOME button fix
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     }
 }
