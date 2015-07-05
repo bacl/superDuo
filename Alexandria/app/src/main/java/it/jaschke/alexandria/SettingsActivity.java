d720bc998c65526a1e08f100734d4b00219544bc
Bruno <baclpt@gmail.com>
2015-07-06 00:43:02 +0100

    Fix settings activity bug
    Show the seteled item summary

--- Alexandria/app/src/main/java/it/jaschke/alexandria/SettingsActivity.java
+++ Alexandria/app/src/main/java/it/jaschke/alexandria/SettingsActivity.java
@@ -1,5 +1,6 @@
 package it.jaschke.alexandria;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -21,12 +22,14 @@ public class SettingsActivity extends PreferenceActivity {
         preference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object o) {
-                return setPreferenceSummary( preference,  o);
+                return setPreferenceSummary(preference, o);
             }
         });
-        setPreferenceSummary(preference,PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), "0"));
+        setPreferenceSummary(preference, PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), "0"));
 
     }
+
+
     public boolean setPreferenceSummary(Preference preference, Object o) {
         String stringValue = o.toString();
         if (preference instanceof ListPreference) {
@@ -39,5 +42,11 @@ public class SettingsActivity extends PreferenceActivity {
             preference.setSummary(stringValue);
         }
         return true;
+    }
+
+    @Override
+    public Intent getParentActivityIntent() {
+        // UP/HOME button fix
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     }
 }
