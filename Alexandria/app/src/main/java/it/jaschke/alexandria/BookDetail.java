29643c0cd0cc9a9259d97d7c00424417c6afb804
Bruno <baclpt@gmail.com>
2015-07-08 10:29:39 +0100

    Bug Fix

--- Alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ Alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -65,6 +65,7 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+        bookTitle=null;
         return rootView;
     }
 
@@ -75,6 +76,17 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
 
         MenuItem menuItem = menu.findItem(R.id.action_share);
         shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        /**
+         *  Bugfix:  in case shareActionProvider isn't set yet on onLoadFinished
+         */
+        if (bookTitle != null) {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+            shareIntent.setType(SHARE_INTENT_TYPE_TEXT_PLAIN);
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text) + bookTitle);
+            shareActionProvider.setShareIntent(shareIntent);
+        }
     }
 
     @Override
@@ -98,11 +110,16 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         bookTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.TITLE));
         ((TextView) rootView.findViewById(R.id.fullBookTitle)).setText(bookTitle);
 
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
-        shareIntent.setType(SHARE_INTENT_TYPE_TEXT_PLAIN);
-        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text) + bookTitle);
-        shareActionProvider.setShareIntent(shareIntent);
+        /**
+         *  Bugfix:  in case shareActionProvider isn't set yet
+         */
+        if (shareActionProvider != null) { //
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+            shareIntent.setType(SHARE_INTENT_TYPE_TEXT_PLAIN);
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text) + bookTitle);
+            shareActionProvider.setShareIntent(shareIntent);
+        }
 
         String bookSubTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.SUBTITLE));
         ((TextView) rootView.findViewById(R.id.fullBookSubTitle)).setText(bookSubTitle);
